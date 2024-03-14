2bd0ad58f173dabdbce0a4e57b4dfda6f743b7d7
huynongquoc05 <160751216+huynongquoc05@users.noreply.github.com>
2024-03-14 07:53:09 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        MP3 mp3 = new MP3("Hay Trao Cho Anh", "A best song", "123", 100f, 240);
+        Book book = new Book("Dragon Ball", "Manga", "456", 30.01f, "Author Name", 300, "Fiction");
 
-        MP3 mp3;
+        System.out.println("Information of MP3:");
         mp3.showInfo();
 
-        Book book;
+        System.out.println("\nInformation of Book:");
         book.showInfo();
-
     }
 }
